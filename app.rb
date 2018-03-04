059e47925a74fc243b3d008d0b256cf4547d53fe
Berry  Semexan <bsemexan@gmail.com>
2018-03-04 02:39:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,4 +4,12 @@ class App < Sinatra::Base
   get '/name' do
     "My name is T'Challa"
   end
+
+  get '/hometown' do
+    "My hometown is Anatua"
+  end
+
+  get 'favorite-song' do 
+    "My favorite song is thong song"
+  end
 end
